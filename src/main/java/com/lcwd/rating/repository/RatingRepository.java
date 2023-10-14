83270b0ee2cdfe76baf5316a08d3124772d33258
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-10-14 18:49:02 +0530

    rating service

---
+++ src/main/java/com/lcwd/rating/repository/RatingRepository.java
@@ -0,0 +1,13 @@
+package com.lcwd.rating.repository;
+
+import com.lcwd.rating.entities.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends MongoRepository<Rating,String>
+{
+    //custom finder methods
+    List<Rating> findByUserId(String userid);
+    List<Rating> findByHotelId(String hotelId);
+}
---
+++ src/main/java/com/lcwd/rating/services/RatingService.java
@@ -0,0 +1,21 @@
+package com.lcwd.rating.services;
+
+import com.lcwd.rating.entities.Rating;
+
+import java.util.List;
+
+public interface RatingService {
+
+    //create
+    Rating create(Rating rating);
+
+    //get all ratings
+    List<Rating> getRatings();
+
+    //get all by UserId
+    List<Rating> getRatingByUserId(String userId);
+
+    //get all by hotel
+    List<Rating> getRatingByHotelId(String hotelId);
+
+}
