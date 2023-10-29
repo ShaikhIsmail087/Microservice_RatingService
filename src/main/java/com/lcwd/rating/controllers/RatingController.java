68190f363c5741aabcf399a4400f4ad39f6cfc99
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-10-30 00:52:20 +0530

    Adding Okta & spring security

--- src/main/java/com/lcwd/rating/controllers/RatingController.java
+++ src/main/java/com/lcwd/rating/controllers/RatingController.java
@@ -5,6 +5,7 @@ import com.lcwd.rating.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class RatingController {
     private RatingService ratingService;
 
     //create rating
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Rating> create(@RequestBody Rating rating){
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
@@ -28,6 +30,7 @@ public class RatingController {
     }
 
     //get all
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> getRatingByUserId(@PathVariable String userId){
         return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
